b70ea2e3cb28001481bdd939483874b35cc5030d
Stoffel van Aswegen <stoffel@zeniton.co.za>
2021-02-09 08:16:00 +0200

    Add API and save photo

---
+++ registrar/api.py
@@ -0,0 +1,31 @@
+
+from registrar import app
+from uuid import uuid1
+from os import path
+from base64 import b64decode
+import numpy as np
+import cv2
+
+
+def identifyMember(photo):
+    photoId, npArr = _saveNewPhoto(photo)
+    member = recogniseMember(npArr)
+    return member, photoId
+
+
+def recogniseMember(npArr):
+    return None
+
+
+def registerMember(member, photoId):
+    pass
+
+
+def _saveNewPhoto(photo):
+    photoId = str(uuid1())
+    filename = path.join(app.config["PHOTOS"], 'tmp', photoId)
+    npArr = np.frombuffer(b64decode(photo), dtype=np.uint8)
+    img = cv2.imdecode(npArr, cv2.IMREAD_UNCHANGED)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(f'{filename}.png', gray)
+    return photoId, npArr
--- registrar/routes.py
+++ registrar/routes.py
@@ -2,11 +2,12 @@ from flask import render_template, url_for, flash, redirect, request, abort
 from registrar import app
 from registrar.forms import RegistrationForm
 from os import path
+import registrar.api
 
 @app.route("/", methods=["GET","POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        # Create image & process
-        pass
+        photo = form.photo.data.split(',')[1]
+        registrar.api.identifyMember(photo)
     return render_template('register.html', title='Register', form=form)
